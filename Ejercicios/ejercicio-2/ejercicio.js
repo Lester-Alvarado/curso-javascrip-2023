60195751e1e03873b407eb47da1d0079d5715578
Lester <lesteraam@gmail.com>
2023-05-04 15:53:27 -0600

    Se agrega Ejercicio 2

---
+++ Ejercicios/ejercicio-2/ejercicio.js
@@ -0,0 +1,15 @@
+const date = [
+  'Lester Alvarado',
+  25,
+  false,
+  new Date(1997, 6, 9),
+    {
+    titulo:"El poder de la mente sub conciente",
+    autor:"Joseph Murphy",
+    fecha: new Date(2019, 6, 02),
+    url: "https://read.amazon.com/kp/embed?asin=B07TSFQCLN&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_2T64MGZV2HAGNQWXNN7P"
+  },
+  ];
+
+
+  console.log(date[4])
