11b6d5fd7e6891f0281c4e46ccb97c80d3e70d28
Lester Alvarado <lesteraam@gmail.com>
2023-05-16 15:27:42 -0600

    se cambian de nombre a ejercicio

---
+++ Ejercicios/ejercicio-7/conjuntos.js
@@ -0,0 +1,19 @@
+let familia = [
+  "Filiberto",
+  "Juana",
+  "Alexander",
+  "Lester",
+  "Blanca",
+  "Evannyelin",
+  "Liam",
+];
+console.log(familia);
+
+let miSet = new Set(familia);
+console.log(miSet);
+
+miSet.add("Lester");
+console.log(miSet);
+
+miSet.add("JavaScript");
+console.log(miSet);
